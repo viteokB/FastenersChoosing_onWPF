9f03cc719207a2fcfa723b965d18b1c030393c4c
viteokB <153222614+viteokB@users.noreply.github.com>
2024-05-19 13:44:56 +0500

    Создал базовый класс Command и LambdaCommand

---
+++ Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastenersChoosing.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private Action<object> _execute;
+
+        private Func<object, bool> _canExucute;
+
+        public LambdaCommand(Action<object> toDo, Func<object, bool> toCheck = null)
+        {
+            this._execute = toDo;
+            this._canExucute = toCheck;
+        }
+        public override bool CanExecute(object? parameter)
+        {
+            return _canExucute == null || _canExucute(parameter);
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
