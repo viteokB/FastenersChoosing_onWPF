b2e10a0dc9018bda20db80eb6ac6b385b3255710
viteokB <153222614+viteokB@users.noreply.github.com>
2024-05-19 12:23:22 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FastenersChoosing
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
