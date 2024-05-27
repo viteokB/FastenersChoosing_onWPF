fbed60a983430a869ed2b07f96aaf81099840761
viteokB <153222614+viteokB@users.noreply.github.com>
2024-05-27 19:25:08 +0500

    Создал DetachableModel -- класс-модель работы с крепежными изделиями

---
+++ Models/DetachableFasteners/DetachableModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using System.Data.OleDb;
+using System.Windows;
+
+namespace FastenersChoosing.Models.DetachableFasteners
+{
+    public class DetachableModel
+    {
+        public static string connectChoose = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = chooseGost.mdb";
+        private OleDbConnection chooseDb;
+        public static string connectGosts = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = Gosts.mdb";
+        private OleDbConnection gostsDb;
+
+        public DetachableModel()
+        {
+            chooseDb = new OleDbConnection(connectChoose);
+            gostsDb = new OleDbConnection(connectGosts);
+        }
+
+    }
+}
