f0c34aaba2e7aea16df2a46fc7752f50d53fc1e9
viteokB <153222614+viteokB@users.noreply.github.com>
2024-05-28 17:01:28 +0500

    Переименовал DetachableModel в DBModel

--- Models/DetachableFasteners/DBModel.cs
+++ Models/DetachableFasteners/DBModel.cs
@@ -4,14 +4,14 @@ using System.Windows;
 
 namespace FastenersChoosing.Models.DetachableFasteners
 {
-    public class DetachableModel
+    public class DBModel
     {
         public static string connectChoose = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = chooseGost.mdb";
         private OleDbConnection chooseDb;
         public static string connectGosts = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = Gosts.mdb";
         private OleDbConnection gostsDb;
 
-        public DetachableModel()
+        public DBModel()
         {
             chooseDb = new OleDbConnection(connectChoose);
             gostsDb = new OleDbConnection(connectGosts);
