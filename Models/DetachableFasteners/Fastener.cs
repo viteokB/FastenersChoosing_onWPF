a849770eb21d98b6bcf77cf791c2e60af5091e62
viteokB <153222614+viteokB@users.noreply.github.com>
2024-06-03 18:12:18 +0500

    Fastener унаследован от BaseViewModel и  все его поля сделаны свойствами(для бинднига в view).

--- Models/DetachableFasteners/Fastener.cs
+++ Models/DetachableFasteners/Fastener.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FastenersChoosing.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,24 +10,61 @@ using System.Windows.Media.Imaging;
 
 namespace FastenersChoosing.Models.DetachableFasteners
 {
-    public class Fastener
+    public class Fastener : BaseViewModel
     {
-        public string Name = null;
+        private string _name;
 
-        public string Type = null;
+        private string _type;
 
-        public string Gost = null;
+        private string _gost;
 
-        public string Description = null;
+        private string _description;
 
-        public BitmapImage Image;
+        private BitmapImage _image;
 
-        public static BitmapImage DefaultImage;
+        public string Name
+        {
+            get => _name;
+            set => Set<string>(ref _name, value);
+        }
+
+        public string Type 
+        {
+            get => _type;
+            set => Set<string>(ref _type, value);
+        }
+
+        public string Gost 
+        {
+            get => _gost;
+            set => Set<string>(ref _gost, value);
+        }
+
+        public string Description 
+        {
+            get => _description;
+            set => Set<string>(ref _description, value);
+        } 
+
+        public BitmapImage Image 
+        {
+            get => _image;
+            set => Set<BitmapImage>(ref _image, value);
+        }
+
+        public static BitmapImage DefaultImage { get; set; }
+
         public Fastener(BitmapImage defaultImage)
         {
             DefaultImage = defaultImage;
 
             Image = DefaultImage;
+        }
+
+        public Fastener(string gost, BitmapImage bi)
+        {
+            Gost = gost;
+            Image = bi;
         }
     }
 }
