460a7a961c9d0547f7865fee323fef52fb4a8f2d
viteokB <153222614+viteokB@users.noreply.github.com>
2024-06-04 22:29:10 +0500

    Доработал батник, теперь копирование в /bin/Data нетолько для x86, но и x64, и AnyCPU

--- copyData.bat
+++ copyData.bat
@@ -1,4 +1,4 @@
-IF NOT EXIST bin\x86\Debug\net8.0-windows\Data\ (
+IF EXIST bin\x86\Debug\net8.0-windows\Data\ (
     mkdir bin\x86\Debug\net8.0-windows\Data 
     xcopy /E /I Data bin\x86\Debug\net8.0-windows\Data\
 ) ELSE (
@@ -11,4 +11,32 @@ IF EXIST bin\x86\Release (
     ) ELSE (
         xcopy /E /I Data bin\x86\Release\net8.0-windows\Data\
     )
-) 
+)
+IF EXIST bin\x64\Debug\net8.0-windows\Data\ (
+    mkdir bin\x64\Debug\net8.0-windows\Data 
+    xcopy /E /I Data bin\x86\Debug\net8.0-windows\Data\
+) ELSE (
+    xcopy /E /I Data bin\x64\Debug\net8.0-windows\Data\
+)
+IF EXIST bin\x64\Release (
+    IF NOT EXIST bin\x64\Release\net8.0-windows\Data\ (
+        mkdir bin\x64\Release\net8.0-windows\Data 
+        xcopy /E /I Data bin\x64\Release\net8.0-windows\Data\
+    ) ELSE (
+        xcopy /E /I Data bin\x64\Release\net8.0-windows\Data\
+    )
+)
+IF EXIST bin\Debug\net8.0-windows\Data\ (
+    mkdir bin\Debug\net8.0-windows\Data 
+    xcopy /E /I Data bin\Debug\net8.0-windows\Data\
+) ELSE (
+    xcopy /E /I Data bin\Debug\net8.0-windows\Data\
+)
+IF EXIST bin\Release (
+    IF NOT EXIST bin\Release\net8.0-windows\Data\ (
+        mkdir bin\Release\net8.0-windows\Data 
+        xcopy /E /I Data bin\Release\net8.0-windows\Data\
+    ) ELSE (
+        xcopy /E /I Data bin\Release\net8.0-windows\Data\
+    )
+)
